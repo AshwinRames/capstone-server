3470db43a051a751fab9c2925ef7f2a8445598d7
AshwinRames <ashwinr178@gmail.com>
2022-04-07 13:03:22 +0530

    Env changed

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 exports.createTransport = nodemailer.createTransport({
     port:465,
     host:"smtp.gmail.com",
     service:"gmail",
     auth:{
-        user:"ashwinr1357@gmail.com",
-        pass:"cabtdsosjbyxjdzf",
+        user:process.env.NODEMAILER_EMAIL,
+        pass:process.env.NODEMAILER_PASS,
     },
     secure: true,
     tls:{
